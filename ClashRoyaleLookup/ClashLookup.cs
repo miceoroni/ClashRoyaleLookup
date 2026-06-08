00cd6e93d780b18e6e0fa1e8e7137bb6915d6047
mice <miceoroni@proton.me>
2026-06-07 20:35:09 -0400

    last commit before readme rewrite

--- ClashRoyaleLookup/ClashLookup.cs
+++ ClashRoyaleLookup/ClashLookup.cs
@@ -1,6 +1,6 @@
-﻿using System.Net.Http.Json;
-using System.Text.Json;
+﻿using System.Text.Json;
 using System.Text.Json.Serialization;
+// ReSharper disable All
 
 namespace ClashRoyaleLookup
 {
@@ -34,7 +34,7 @@ namespace ClashRoyaleLookup
 
         public class PlayerProfile
         {
-            public Clan clan { get; set; }
+            public Clan? clan { get; set; }
             public string? name { get; set; }
             public int expLevel { get; set; }
             public int trophies { get; set; }
@@ -43,16 +43,16 @@ namespace ClashRoyaleLookup
             public int losses { get; set; }
             public int battleCount { get; set; }
             public int threeCrownWins { get; set; }
-            public CurrentFavouriteCard currentFavouriteCard { get; set; }
+            public CurrentFavouriteCard? currentFavouriteCard { get; set; }
     }
 
         public class Clan
         {
             [JsonPropertyName("tag")]
-            public string Tag { get; set; }
+            public string? Tag { get; set; }
             
             [JsonPropertyName("name")]
-            public string Name { get; set; }
+            public string? Name { get; set; }
             
             [JsonPropertyName("badgeId")]
             public int BadgeId { get; set; }
@@ -61,10 +61,10 @@ namespace ClashRoyaleLookup
         public class ClanBadge
         {
             [JsonPropertyName("name")]
-            public string Name { get; set; }
+            public string? Name { get; set; }
             
             [JsonPropertyName("icon_export_name")]
-            public string IconExportName { get; set; }
+            public string? IconExportName { get; set; }
             
             [JsonPropertyName("id")]
             public int Id { get; set; }
@@ -79,14 +79,14 @@ namespace ClashRoyaleLookup
         }
         public class CurrentFavouriteCard
         {
-            public CardIconUrls iconUrls { get; set; }
+            public CardIconUrls? iconUrls { get; set; }
             
-            public string name { get; set; }
+            public string? name { get; set; }
             
             public int id { get; set; }
-            
-            public string rarity { get; set; }
-            
+
+            public string? rarity { get; set; }
+
             public int maxLevel { get; set; }
             
             public int elixirCost { get; set; }
@@ -100,37 +100,7 @@ namespace ClashRoyaleLookup
             using FileStream stream = File.OpenRead(path);
             var list = await JsonSerializer.DeserializeAsync<List<ClanBadge>>(stream);
             
-            return list?.FirstOrDefault(b => b.Id == BadgeId);
+            return list?.FirstOrDefault(b => b.Id == BadgeId) ?? throw new InvalidOperationException();
         }
     }
 }
-/*
-public static class ClashLookupProgram
-{
-    public static async Task Main(string[] args)
-    {
-        Console.WriteLine("ClashRoyaleLookup - Written by Mice");
-        Console.WriteLine("https://github.com/miceoroni/ClashRoyaleLookup");
-        while (true)
-        {
-            Console.WriteLine("Enter the clash user ID you wish to find: ");
-            var playerTag = Console.ReadLine();
-
-            if (string.Equals(playerTag.Trim(), "exit", StringComparison.OrdinalIgnoreCase))
-            {
-                Console.WriteLine("Exiting...");
-                return;
-            }
-            if (!string.IsNullOrWhiteSpace(playerTag))
-            {
-                await ClashLookup.SearchForUser(playerTag);
-            }
-            else
-            {
-                Console.WriteLine("You need to enter a tag.");
-            }
-        }
-    }
-}
-}
-*/
--- ClashRoyaleLookup/Program.cs
+++ ClashRoyaleLookup/Program.cs
@@ -2,8 +2,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddHttpClient();
 
-builder.Services.AddSingleton(builder.Configuration["ClashRoyaleApiKey"]!);
+builder.Services.AddSingleton(builder.Configuration["ClashRoyaleApiKey"]);
 var app = builder.Build();
+app.UseDefaultFiles();
 app.UseStaticFiles();
 app.MapControllers();
 app.Run();
